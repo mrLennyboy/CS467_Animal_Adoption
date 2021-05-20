167ea0ccbcbac087b77d0d11be4ab04b9f4747ab
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2021-05-20 16:21:30 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,9 +3,9 @@ pets = "pets"
 breeds = "breeds"
 
 BUCKET = "https://storage.googleapis.com/datingappforanimaladoption.appspot.com/uploads/"
-SECRET_KEY = r'jdkjivjsn43mdkos987slkjd'
-CLIENT_ID = r'939115278036-he2m51te7ohrp1m9r457nos1dbnh5u2o.apps.googleusercontent.com'
-CLIENT_SECRET = r'LQQ_RyrsV-eA1uiuux0RrI7J'
+SECRET_KEY = #
+CLIENT_ID = #
+CLIENT_SECRET = #
 SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
 #REDIRECT_URI = 'https://datingappforanimaladoption.wl.r.appspot.com/authorization'
 REDIRECT_URI = 'http://localhost:8080/authorization'
